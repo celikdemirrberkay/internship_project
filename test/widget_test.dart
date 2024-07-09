ceb168e4ac909f98245aa1af087240a99ab0845f
Berkay Çelikdemir <berkaycelikdemirr@gmail.com>
2024-07-09 10:21:42 +0300

    All set for other features

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:internship_project/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PrayerTime());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
