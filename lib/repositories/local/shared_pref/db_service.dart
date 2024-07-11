fc0fe2b36338bbd3e4fda0e450dcb3d285afc308
Berkay Çelikdemir <berkaycelikdemirr@gmail.com>
2024-07-11 15:31:56 +0300

    Db service successfully created

--- lib/repositories/local/shared_pref/db_service.dart
+++ lib/repositories/local/shared_pref/db_service.dart
@@ -1,16 +1,17 @@
-import 'package:either_dart/src/either.dart';
 import 'package:hive/hive.dart';
+import 'package:either_dart/src/either.dart';
 import 'package:internship_project/repositories/local/shared_pref/db_service_interface.dart';
 
+/// Abstraction of local database service
 class DatabaseService extends IDatabaseService {
   /// Constructor
-  DatabaseService(super.hive);
+  DatabaseService();
 
   @override
-  Future<Either<String, String?>> get(String key) async {
+  Future<Either<String, int?>> get(String key) async {
     try {
       /// Hive box
-      final box = await hive.openBox<String>(DbServiceEnum.databaseService.name);
+      final box = await Hive.openBox<int>(DbServiceEnum.databaseService.name);
       final response = box.get(key);
 
       return Right(response);
@@ -23,7 +24,7 @@ class DatabaseService extends IDatabaseService {
   Future<Either<String, String>> remove(String key) async {
     try {
       /// Hive box
-      final box = await hive.openBox<String>(DbServiceEnum.databaseService.name);
+      final box = await Hive.openBox<String>(DbServiceEnum.databaseService.name);
       await box.delete(key);
 
       return Right('Başarıyla silindi.');
@@ -36,7 +37,7 @@ class DatabaseService extends IDatabaseService {
   Future<Either<String, String>> set(String key, int value) async {
     try {
       /// Hive box
-      final box = await hive.openBox<int>(DbServiceEnum.databaseService.name);
+      final box = await Hive.openBox<int>(DbServiceEnum.databaseService.name);
       await box.put(key, value);
 
       return Right('Başarıyla kaydedildi');
--- lib/repositories/local/shared_pref/db_service_interface.dart
+++ lib/repositories/local/shared_pref/db_service_interface.dart
@@ -1,16 +1,9 @@
 import 'package:either_dart/either.dart';
-import 'package:hive/hive.dart';
 
 /// Abstraction of local database service
 abstract class IDatabaseService {
-  /// Constructor
-  IDatabaseService(this.hive);
-
-  /// Hive interface instance
-  final HiveInterface hive;
-
   /// Get the value of the key from the shared preferences.
-  Future<Either<String, String?>> get(String key);
+  Future<Either<String, int?>> get(String key);
 
   /// Set the value of the key in the shared preferences.
   Future<Either<String, String>> set(String key, int value);
