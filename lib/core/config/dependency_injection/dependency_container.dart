adb5bdd109f120650ca8112add126bfe969347ed
Berkay Çelikdemir <berkaycelikdemirr@gmail.com>
2024-07-11 14:26:53 +0300

    All conflicts solved

--- lib/core/config/dependency_injection/dependency_container.dart
+++ lib/core/config/dependency_injection/dependency_container.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import 'package:internship_project/repositories/god_names/god_names_service.dart';
-import 'package:internship_project/repositories/prayer_times/prayer_times_service.dart';
+import 'package:internship_project/repositories/local/god_names/god_names_service.dart';
+import 'package:internship_project/repositories/remote/prayer_times/prayer_times_service.dart';
 
 /// The locator class where we register our dependencies
 final locator = GetIt.instance;
