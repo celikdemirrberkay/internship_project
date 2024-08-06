331a80fcbafbeb0881e08b456216a0a33f78780a
Berkay Çelikdemir <berkaycelikdemirr@gmail.com>
2024-08-06 10:24:16 +0300

    Manifest file refactored for fatal exception

--- android/app/src/main/java/com/example/internship_project/HomeWidget.kt
+++ android/app/src/main/java/com/example/internship_project/HomeWidget.kt
@@ -17,7 +17,12 @@ class HomeWidget : AppWidgetProvider() {
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            val widgetData = HomeWidgetPlugin.getData(context)
+            val views = RemoteViews(context.packageName, R.layout.home_widget).apply{
+                val title = widgetData.getString("headline_title",null)
+                setTextViewText(R.id.headline_title, title ?: "Başlık bulunamadı")
+            }
+            appWidgetManager.updateAppWidget(appWidgetId,views)
         }
     }
 
@@ -38,7 +43,7 @@ internal fun updateAppWidget(
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.home_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    views.setTextViewText(R.id.headline_title, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
