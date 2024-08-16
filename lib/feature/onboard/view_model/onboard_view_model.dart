9acae2594bfd30579b244790d98c2eaf17550329
Berkay Çelikdemir <berkaycelikdemirr@gmail.com>
2024-08-16 13:11:31 +0300

    Unused import removed

--- lib/feature/onboard/view_model/onboard_view_model.dart
+++ lib/feature/onboard/view_model/onboard_view_model.dart
@@ -1,5 +1,3 @@
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:internship_project/core/base/resource.dart';
 import 'package:internship_project/core/constants/local_database_constants.dart';
 import 'package:internship_project/service/local/hive/db_service.dart';
 import 'package:stacked/stacked.dart';
@@ -15,17 +13,10 @@ class OnboardViewModel extends BaseViewModel {
   /// Set onboard done
   Future<void> setOnboardDone() async {
     /// Set onboard done to local database
-    final response = await localDatabaseService.set<bool>(
+    await localDatabaseService.set<bool>(
       dbName: LocalDatabaseNames.onboardDB.value,
       key: LocalDatabaseKeys.isOnboardDone.value,
       value: true,
     );
-
-    /// If set operation success
-    if (response is SuccessState<String>) {
-      await Fluttertoast.showToast(
-        msg: response.data!,
-      );
-    }
   }
 }
